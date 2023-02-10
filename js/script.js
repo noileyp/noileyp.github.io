9cef18cb8453f80b76fb1d6ce5f04d7f640323f7
noileyp <nolankpeterson@gmail.com>
2023-02-09 20:17:22 -0600

    v3

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var imageThumbs = document.getElementById("image-thumbs");
+var currentImage = document.getElementById("current-image");
+
+for (var i = 1; i <= 6; i++) {
+  var thumb = document.createElement("img");
+  thumb.src = "https://nkpphotography.s3.amazonaws.com/" + i + ".jpg";
+  thumb.alt = "Image " + i;
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
+  thumb.addEventListener(
+    "click", function() {
+      currentImage.src = this.src;
+    }
+  );
+}
