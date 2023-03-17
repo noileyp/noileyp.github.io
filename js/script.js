a8e80bd3f8a5e1a385b567504df63d3495c320a9
noileyp <nolankpeterson@gmail.com>
2023-03-16 22:12:45 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var imageThumbs = document.getElementById("image-thumbs");
 var currentImage = document.getElementById("current-image");
 
-for (var i = 1; i <= 6; i++) {
+for (var i = 1; i <= 7; i++) {
   var thumb = document.createElement("img");
   thumb.src = "https://nkpphotography.s3.amazonaws.com/" + i + ".jpg";
   thumb.alt = "Image " + i;
