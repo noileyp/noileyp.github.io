6639450bc69fe38f9de5915f98a0fdbb81517a1e
noileyp <nolankpeterson@gmail.com>
2023-03-19 16:12:35 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 var imageThumbs = document.getElementById("image-thumbs");
 var currentImage = document.getElementById("current-image");
 
+//I need to change the for loop below to not stop at a hardcoded value but rather the number of photos in the S3 bucket.
+
 for (var i = 1; i <= 7; i++) {
   var thumb = document.createElement("img");
   thumb.src = "https://nkpphotography.s3.amazonaws.com/" + i + ".jpg";
